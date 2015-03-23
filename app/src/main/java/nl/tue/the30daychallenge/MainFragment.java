4881d37eec8789011d1c9a1f19d5cc08ce4a20a6
Kagan Akbas <akbask@gmail.com>
2015-03-23 12:28:40 +0100

    Changed the name preferences fragment to main

--- app/src/main/java/nl/tue/the30daychallenge/MainFragment.java
+++ app/src/main/java/nl/tue/the30daychallenge/MainFragment.java
@@ -6,8 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class PreferencesFragment extends Fragment {
-    public PreferencesFragment() {
+public class MainFragment extends Fragment {
+    public MainFragment() {
         // Required empty public constructor
     }
 
