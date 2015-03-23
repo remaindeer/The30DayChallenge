192fea83904a27a4d53ee3bea820662063150c45
Kagan Akbas <akbask@gmail.com>
2015-03-23 12:57:04 +0100

    Renaming fragments

--- app/src/main/java/nl/tue/the30daychallenge/MainFragment.java
+++ app/src/main/java/nl/tue/the30daychallenge/MainFragment.java
@@ -16,6 +16,6 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_preferences, container, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
