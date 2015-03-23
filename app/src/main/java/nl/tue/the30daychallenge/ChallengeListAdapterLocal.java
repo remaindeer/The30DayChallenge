5f0bc88a35a08349e0e4b4739b5c6e2ed43be705
Kagan Akbas <akbask@gmail.com>
2015-03-23 16:38:05 +0100

    Added new Challenge List adapter, configured main screen fragment

---
+++ app/src/main/java/nl/tue/the30daychallenge/ChallengeListAdapterLocal.java
@@ -0,0 +1,82 @@
+package nl.tue.the30daychallenge;
+
+/**
+ * Created by s122552 on 21-3-2015.
+ */
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import nl.tue.the30daychallenge.data.LocalChallenge;
+import nl.tue.the30daychallenge.data.RemoteChallenge;
+
+public class ChallengeListAdapterLocal extends ArrayAdapter {
+
+    private Context context;
+    private boolean useList = true;
+
+    public ChallengeListAdapterLocal(Context context, List items) {
+        super(context, android.R.layout.simple_list_item_1, items);
+        this.context = context;
+    }
+
+    /**
+     * Holder for the list items.
+     */
+    private class ViewHolder{
+        TextView titleText;
+        TextView descriptionText;
+        TextView downloadsText;
+        TextView completionsText;
+    }
+
+    /**
+     *
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return
+     */
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        LocalChallenge item = (LocalChallenge)getItem(position);
+        View viewToUse = null;
+
+        // This block exists to inflate the settings list item conditionally based on whether
+        // we want to support a grid or list view.
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            if(useList){
+                viewToUse = mInflater.inflate(R.layout.challenge_list_item, null);
+            } else {
+                viewToUse = mInflater.inflate(R.layout.challenge_grid_item, null);
+            }
+
+            holder = new ViewHolder();
+            holder.titleText = (TextView)viewToUse.findViewById(R.id.titleTextView);
+            holder.descriptionText = (TextView)viewToUse.findViewById(R.id.descriptionTextView);
+            holder.downloadsText = (TextView)viewToUse.findViewById(R.id.downloadsTextView);
+            holder.completionsText = (TextView)viewToUse.findViewById(R.id.completionsTextView);
+
+            viewToUse.setTag(holder);
+        } else {
+            viewToUse = convertView;
+            holder = (ViewHolder) viewToUse.getTag();
+        }
+
+        holder.titleText.setText(item.title);
+        holder.descriptionText.setText(item.description);
+
+
+
+        return viewToUse;
+    }
+}
--- app/src/main/java/nl/tue/the30daychallenge/MainFragment.java
+++ app/src/main/java/nl/tue/the30daychallenge/MainFragment.java
@@ -1,21 +1,109 @@
 package nl.tue.the30daychallenge;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.Toast;
 
-public class MainFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+import nl.tue.the30daychallenge.data.LocalChallenge;
+import nl.tue.the30daychallenge.data.LocalConnector;
+
+
+public class MainFragment extends Fragment implements AbsListView.OnItemClickListener, AbsListView.OnScrollListener {
+
+    private List challengeListItemList = new ArrayList(); // at the top of your fragment list
+    private int page = 0;
+    private int itemspage= 15;
+
+    /**
+     * The fragment's ListView/GridView.
+     */
+    private AbsListView mListView;
+
+    /**
+     * The Adapter which will be used to populate the ListView/GridView with
+     * Views.
+     */
+    private ListAdapter mAdapter;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
     public MainFragment() {
-        // Required empty public constructor
+
+
     }
 
+    public void getChallenges(){
+        challengeListItemList = LocalConnector.getChallenges();
+        ((ChallengeListAdapterLocal)mAdapter).notifyDataSetChanged();
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*
+        challengeListItemList = new ArrayList();
+        challengeListItemList.add(new ChallengeListItem("Challenge", R.mipmap.category_icon_chores));
+        challengeListItemList.add(new ChallengeListItem("Creativity", R.mipmap.category_icon_creativity));
+        challengeListItemList.add(new ChallengeListItem("Finance", R.mipmap.category_icon_finance));
+        challengeListItemList.add(new ChallengeListItem("Fitness", R.mipmap.category_icon_fitness));
+        challengeListItemList.add(new ChallengeListItem("Funny", R.mipmap.category_icon_funny));
+        challengeListItemList.add(new ChallengeListItem("Intellectual", R.mipmap.category_icon_intellectual));
+        challengeListItemList.add(new ChallengeListItem("Mental health", R.mipmap.category_icon_mental_health));
+        challengeListItemList.add(new ChallengeListItem("Outgoing", R.mipmap.category_icon_outgoing));
+        challengeListItemList.add(new ChallengeListItem("Relationship", R.mipmap.category_icon_relationship));
+        challengeListItemList.add(new ChallengeListItem("Social", R.mipmap.category_icon_social));
+        */
+        challengeListItemList.add(new LocalChallenge("Challenge", "describe", 1));
+        mAdapter = new ChallengeListAdapterLocal(getActivity(), challengeListItemList);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_catitem, container, false);
+        Log.d("Store","load view");
+        // Set the adapter
+        mListView = (AbsListView) view.findViewById(android.R.id.list);
+        ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
+
+        // Set OnItemClickListener so we can be notified on item clicks
+        mListView.setOnItemClickListener(this);
+        mListView.setOnScrollListener(this);
+
+        return view;
+    }
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        LocalChallenge item = (LocalChallenge) this.challengeListItemList.get(position);
+        Toast.makeText(getActivity(), item.title + " Clicked!"
+                , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        if(firstVisibleItem+visibleItemCount==totalItemCount &&firstVisibleItem > 0 && totalItemCount == (page+1)*itemspage){
+            page++;
+            getChallenges();
+        }
     }
 }
